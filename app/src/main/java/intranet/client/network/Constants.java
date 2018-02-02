d6e67f0184d63c49ca041dedb93ed67a7033133e
francisting <franciskamau2004@gmail.com>
2018-02-02 09:52:40 +0300

    Initial commit

---
+++ app/src/main/java/intranet/client/network/Constants.java
@@ -0,0 +1,7 @@
+package intranet.client.network;
+
+public class Constants {
+    static final String GRAILS_APP_URL = "http://10.0.2.2:8080/"; // <1>
+    static final String ANNOUNCEMENTS_PATH = "announcements"; // <2>
+    static final String ACCEPT_VERSION = "1.0"; // <3>
+}
